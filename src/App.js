8106d899a9d365fcd92fbe804914cc99fd371037
Claire Bohlen <clairebohlen@gmail.com>
2020-11-25 10:34:50 -0700

    more importing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
 import './css/App.css';
+import About from "./components/About";
+import Connect from "./components/Connect";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+
 
 function App() {
   return (
