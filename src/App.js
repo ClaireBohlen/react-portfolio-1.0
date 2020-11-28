107cced627afcd896b52eccaca92f35b3c9a0a8b
Claire Bohlen <clairebohlen@gmail.com>
2020-11-28 14:58:32 -0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Connect from "./components/Connect";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from "react-router-dom"
 
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-      <Projects/>
-      <About/>
-      <Connect/>
+    <>
+      <Router>
+        <Navbar/>
+        {/* <Home/>
+        <Projects/>
+        <About/>
+        <Connect/> */}
+      </Router>
 
-    </div>
+    </>
     
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo
+} from "./NavbarElements"
 
 const Navbar = () => {
     return (
